a6084fb6465678eb5089faa108fb88652427f7f8
Jorge2610 <jorge261093@gmail.com>
2023-04-13 19:22:23 -0400

    100x100px or above restriction for image upload.

--- client/src/Forms/ComponentesInventario/Upload.js
+++ client/src/Forms/ComponentesInventario/Upload.js
@@ -18,7 +18,6 @@ const App = ({ getImgUrlUpload, imagenUrl }) => {
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
   const [previewTitle, setPreviewTitle] = useState('');
-  const [validImage, setValidImage] = useState(true);
   const [loading, setLoaging] = useState(false);
   const [fileList, setFileList] = useState(imagenUrl !== "Sin imagen" ? [{
     uid: '-1',
@@ -39,25 +38,7 @@ const App = ({ getImgUrlUpload, imagenUrl }) => {
     setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
   };
 
-  const uploadButton = (
-    <div>
-      {loading ? <LoadingOutlined /> : <PlusOutlined />}
-      <div
-        style={{
-          marginTop: 8,
-        }}
-      >
-        Subir
-      </div>
-    </div>
-  );
-
   const handleChange = (fileList) => {
-    if (validImage) {
-      setValidImage(false);
-    } else {
-      fileList["fileList"].pop();
-    }
     setFileList(fileList["fileList"]);
   };
 
@@ -68,26 +49,47 @@ const App = ({ getImgUrlUpload, imagenUrl }) => {
 
   const beforeUpload = async (file) => {
     const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-    const isLt2M = file.size / 1024 / 1024 < 2;
-    console.log(file.type.indexOf("image"));
     if (!isJpgOrPng) {
       message.error('Solo se admiten imagenes JPG/PNG!');
-      setValidImage(false);
-      handleCancel();
-    } else {
-      if (!isLt2M) {
-        message.error('El peso maximo de la imagen debe ser de 2MB!');
-        setValidImage(false);
-        handleCancel();
-      } else {
-        setLoaging(true);
-        await uploadImage(file);
-        setLoaging(false);
-      }
+      return Upload.LIST_IGNORE;
     }
-    setValidImage(isJpgOrPng && isLt2M);
+
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('El peso maximo de la imagen debe ser de 2MB!');
+      return Upload.LIST_IGNORE;
+    }
+
+    const isPl100P = await validateImageDimensions(file);
+    if (!isPl100P) {
+      message.error("La imagen debe ser de almenos 100x100 pixeles!");
+      return Upload.LIST_IGNORE;
+    }
+
+    setLoaging(true);
+    await uploadImage(file);
+    setLoaging(false);
     return false;
   };
+
+  const validateImageDimensions = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = function (e) {
+        var image = new Image();
+        image.src = e.target.result;
+        image.onload = function () {
+          var height = this.height;
+          var width = this.width;
+          if (height < 100 || width < 100) {
+            resolve(false);
+          }
+          resolve(true);
+        };
+      };
+      reader.onerror = (error) => reject(error);
+    });
 
   const uploadImage = async (file) => {
     const formData = new FormData();
@@ -112,6 +114,19 @@ const App = ({ getImgUrlUpload, imagenUrl }) => {
       message.error('No se pudo subir la imagen, intente nuevamente.');
     }
   };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Subir
+      </div>
+    </div>
+  );
 
   return (
     <>
